7631c20ecde775a5ca97ab3b3776b34e8dda3cc4
dabogdan <39666410+dabogdan@users.noreply.github.com>
2021-01-27 11:24:43 +0200

    touch left&right buttons added

--- sketch.js
+++ sketch.js
@@ -59,19 +59,19 @@ function startGame(){
     collectables = [
         {
             x_pos: 400, 
-            y_pos: floorPos_y-10, 
+            y_pos: floorPos_y-70, 
             size: 30,
             isFound: false
         },
         {
             x_pos: 900, 
-            y_pos: floorPos_y-10, 
+            y_pos: floorPos_y-100, 
             size: 30,
             isFound: false
         },
         {
             x_pos: 120, 
-            y_pos: floorPos_y-10, 
+            y_pos: floorPos_y-50, 
             size: 30,
             isFound: false
         }
@@ -110,7 +110,7 @@ function startGame(){
 
 function setup()
 {
-	createCanvas(1024, 576);
+	createCanvas(windowWidth, windowHeight);
 	floorPos_y = height * 3/4;
    
     lives = 3;
@@ -132,7 +132,7 @@ function draw()
     fill(255);
     
     fill(181,101,29);
-    triangle(0, floorPos_y+50, 10, floorPos_y+40, 20,floorPos_y+50);
+//    triangle(0, floorPos_y+50, 10, floorPos_y+40, 20,floorPos_y+50);
     rect(0, floorPos_y+50, width, height/4); // draw some soil
     
     
@@ -177,6 +177,21 @@ function draw()
         checkPlayerDie();
     }
     
+//    attempt to make a night
+//    var opacity = map(0,10, 1, 0.2)
+//    fill('rgba(0,0,0,.4)');
+//    rect(-scrollPos,0,width+scrollPos,height);
+    
+    //touch screen buttons
+    fill(0);
+    ellipse(gameChar_world_x-48, floorPos_y + 70, 45);
+    ellipse(gameChar_world_x+48, floorPos_y + 70, 45);
+    
+    fill(255);
+    triangle (gameChar_world_x-50, floorPos_y + 80, gameChar_world_x-60, floorPos_y + 70, gameChar_world_x-50, floorPos_y + 60); //left
+    triangle (gameChar_world_x-40, floorPos_y + 80, gameChar_world_x-50, floorPos_y + 70, gameChar_world_x-40, floorPos_y + 60);
+    triangle (gameChar_world_x+50, floorPos_y + 80, gameChar_world_x+60, floorPos_y + 70, gameChar_world_x+50, floorPos_y + 60); //right
+    triangle (gameChar_world_x+40, floorPos_y + 80, gameChar_world_x+50, floorPos_y + 70, gameChar_world_x+40, floorPos_y + 60);
     
     pop();
 
@@ -223,7 +238,7 @@ function draw()
 	// Logic to make the game character move or the background scroll.
 	if(isLeft)
 	{
-		if(gameChar_x > width * 0.2 && isPlummeting == false)
+		if(gameChar_x > width/2 && isPlummeting == false)
 		{
 			gameChar_x -= 5;
 		}
@@ -235,9 +250,9 @@ function draw()
 
 	if(isRight)
 	{
-		if(gameChar_x < width * 0.8 && isPlummeting == false)
+		if(gameChar_x < width/2 && isPlummeting == false)
 		{
-			gameChar_x  += 5;
+			gameChar_x += 5;
 		}
 		else
 		{
@@ -279,9 +294,6 @@ function draw()
 // ---------------------
 
 function keyPressed(){
-
-	console.log("press" + keyCode);
-	console.log("press" + key);
     
     // if statements to control the animation of the character when
 	// keys are pressed.
@@ -299,11 +311,7 @@ function keyPressed(){
 }
 
 function keyReleased()
-{
-
-	console.log("release" + keyCode);
-	console.log("release" + key);
-    
+{   
     	// if statements to control the animation of the character when
 	// keys are released.
     if (keyCode == 37) {
@@ -317,6 +325,22 @@ function keyReleased()
         isJumping = false;
     }
     
+}
+
+function touchStarted(event) {
+  
+    if (dist(event.clientX, event.clientY, width/2-48, floorPos_y + 70) < 45) {
+        isLeft = true;
+    }
+    if (dist(event.clientX, event.clientY, width/2+48, floorPos_y + 70) < 45) {
+        isRight = true;
+    }
+    return false;
+}
+
+function touchEnded(event){
+    isLeft = false;
+    isRight = false;
 }
 
 
@@ -443,6 +467,24 @@ function drawGameChar()
 		// add your standing front facing code
         fill(235, 240, 165);
         ellipse(gameChar_x,gameChar_y-50,20); //head
+        
+        stroke(0);
+        strokeWeight(.6);
+        fill(255);
+        ellipse(gameChar_x-5,gameChar_y-51,7); //left eye
+        fill(0);
+        ellipse(gameChar_x-5,gameChar_y-51,1); //apple of the left eye
+        fill(255);
+        ellipse(gameChar_x+5,gameChar_y-51,7); //right eye
+        fill(0);
+        ellipse(gameChar_x+5,gameChar_y-51,1); //apple of the right eye
+        
+        fill(250, 128, 114);
+        ellipse(gameChar_x,gameChar_y-44,10,2); //mouth
+        
+        
+        noStroke();
+        fill(235, 240, 165);
         rect(gameChar_x-10,gameChar_y-40,-5,20); //left hand
         rect(gameChar_x+10,gameChar_y-40,+5,20); //right hand
 
@@ -509,9 +551,9 @@ function drawTrees ()
         rect(trees_x[i],floorPos_y-45,10,45); // tree trunk
 
         fill(21, 87, 47); //green
-        triangle(trees_x[i]-25,402,trees_x[i]+5,350,trees_x[i]-35+70,402); //lower branch
-        triangle(trees_x[i]-25,402-30,trees_x[i]+5,350-30,trees_x[i]-35+70,402-30); //middle branch
-        triangle(trees_x[i]-25,402-30-30,trees_x[i]+5,350-30-30,trees_x[i]-35+70,402-30-30);//higher branch
+        triangle(trees_x[i]-25,floorPos_y-40,trees_x[i]+5,floorPos_y-90,trees_x[i]-35+70,floorPos_y-40); //lower branch
+        triangle(trees_x[i]-25,floorPos_y-40-30,trees_x[i]+5,floorPos_y-90-30,trees_x[i]-35+70,floorPos_y-40-30); //middle branch
+        triangle(trees_x[i]-25,floorPos_y-40-30-30,trees_x[i]+5,floorPos_y-90-30-30,trees_x[i]-35+70,floorPos_y-40-30-30);//higher branch
     }
 }
 
@@ -525,11 +567,12 @@ function drawCanyon(t_canyon)
 {    
     //draw the canyon
     fill(102,102,153);
-    rect(t_canyon.x_pos, 432, t_canyon.width, 144);//borders of the canyon
-    fill(100,155,255);
-    rect(t_canyon.x_pos + 20, 550, t_canyon.width - 40, 26);//water
+    rect(t_canyon.x_pos, floorPos_y, t_canyon.width, height);//borders of the canyon
     fill(255,215,0);
-    rect(t_canyon.x_pos + 20, 432, t_canyon.width - 40, 118);//yellow space for the character to fall within
+    rect(t_canyon.x_pos + 20, floorPos_y, t_canyon.width - 40, height);//yellow space for the character to fall within
+    fill(100,155,255);
+    rect(t_canyon.x_pos + 20, floorPos_y+140, t_canyon.width - 40, height);//water
+    
     
 }
 
@@ -573,7 +616,7 @@ function drawCollectable(t_collectable)
 
 function checkCollectable(t_collectable)
 {
-    if(dist(gameChar_world_x, gameChar_y, t_collectable.x_pos, t_collectable.y_pos) < 35){
+    if(dist(gameChar_world_x, gameChar_y, t_collectable.x_pos, t_collectable.y_pos) < 40 || dist(gameChar_world_x ,gameChar_y-50, t_collectable.x_pos, t_collectable.y_pos) < 30){
         t_collectable.isFound = true;
         game_score +=1;
     }
